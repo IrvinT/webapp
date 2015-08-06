4fbee09082d150ac8b35f6724c4079464293962b
Irvin <irvinthomas@outlook.fr>
2015-08-06 19:54:29 +0200

    Modification du header (compatibilité w3c)

--- app/controller/Front.php
+++ app/controller/Front.php
@@ -10,14 +10,14 @@ class Front {
 
     public function __construct()
     {
-    	$this->render = new Engine();
+        $this->render = new Engine();
 
-    	$this->render->register('view', 'Smarty', array(), function($smarty){
-			$smarty->template_dir = 'app/views/';
-			$smarty->compile_dir = 'app/tmp/';
-			$smarty->config_dir = 'app/config/';
-			$smarty->cache_dir = 'app/cache/';
-		});
+        $this->render->register('view', 'Smarty', array(), function($smarty){
+            $smarty->template_dir = 'app/views/';
+            $smarty->compile_dir = 'app/tmp/';
+            $smarty->config_dir = 'app/config/';
+            $smarty->cache_dir = 'app/cache/';
+        });
 
         $this->render->set('app.base_uri', 'app/');
         $this->render->set('app.bower_uri', 'bower_components/');
@@ -30,7 +30,6 @@ class Front {
 
     public function home()
     {
-    	$this->render->view()->display('layout.tpl');
+        $this->render->view()->display('layout.tpl');
     }
 }
-?>
--- app/views/layout.tpl
+++ app/views/layout.tpl
@@ -1,8 +1,3 @@
-<!-- Include header layout -->
 {include file='header.tpl'}
-
-<!-- Include base layout -->
 {include file='base.tpl'}
-
-<!-- Include footer layout -->
 {include file='footer.tpl'}
